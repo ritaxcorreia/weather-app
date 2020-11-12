541461271a73c60dffb482885ec3e17c21a52f13
Rita <rita@ritacorreia.co>
2020-11-12 15:34:58 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,6 +36,8 @@ function showTemp(data) {
         <p>Humidity: ${humidity}%</p>
         <p>Visibility: ${visibility}km</p>
         `;
+
+	openWeatherIcon(data);
 }
 
 // Data fetch from OpenWeatherMap API, displays London weather as default
@@ -47,48 +49,13 @@ fetch(weatherUrl)
 	})
 	.then((data) => {
 		console.log(data);
-		// FAHRENHEIT: calculate and store Fahrenheit temperature
-		const degFahrenheit = (data.main.temp - 273.15) * 1.8 + 32;
-		// // round calculated temperature down to the nearest whole number
-		const degFahrenheitInteger = Math.floor(degFahrenheit);
-
-		// // CELSIUS: calculate and store Celsius temperature
-		const degCelsius = (degFahrenheitInteger - 32) / 1.8;
-		const degCelsiusInteger = Math.floor(degCelsius);
-
-		const feelsLikeFahrenheit = Math.floor(
-			(data.main.feels_like - 273.15) * 1.8 + 32
-		);
-		const feelsLikeCelsius = Math.floor((feelsLikeFahrenheit - 32) / 1.8);
-		const clouds = data.weather[0].main;
-		const typeOfClouds = data.weather[0].description;
-		const humidity = data.main.humidity;
-		const visibility = data.visibility / 1000;
-
-		console.log(
-			`Temperature for ${data.name} in Kelvin: ${data.main.temp}°K`
-		);
-		console.log(
-			`Temperature for ${data.name} in Fahrenheit: ${degFahrenheitInteger}°F`
-		);
-		console.log(
-			`Temperature for ${data.name} in Celsius: ${degCelsiusInteger}°C`
-		);
-
-		tempSpan.innerHTML = `<h2>London</h2>
-        <h3>${degCelsiusInteger}°C / ${degFahrenheitInteger}°F</h3>
-        <p>Feels like ${feelsLikeCelsius}°C / ${feelsLikeFahrenheit}°F</p>
-        <p>${clouds}, ${typeOfClouds}</p>
-        <p>Humidity: ${humidity}%</p>
-        <p>Visibility: ${visibility}km</p>
-        `;
-
-		openWeatherIcon(data);
+		showTemp(data);
 	});
 
 // Users type in City, press Get Weather button and returns data for the city specified
 
 getTemp.addEventListener("click", function () {
+	event.preventDefault();
 	const cityText = cityInputField.value;
 
 	const cityWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityText}&appid=1680f86f99f9722f2e92efbfac0afa4a`;
@@ -102,9 +69,6 @@ getTemp.addEventListener("click", function () {
 			console.log("Custom city:", data);
 
 			showTemp(data);
-
-			//shows weather icon
-			openWeatherIcon(data);
 		});
 });
 
